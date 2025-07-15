e124541241d11a8e3e358c8e2570481c50fae552
Math Tumba <tumbarello.math@gmail.com>
2025-07-15 12:27:47 +0200

    Fix : - Suppression du script reloadPaginationFriendRequests désormais inutile avec les live components - Suppression des requêtes dupliquées sur la page des demandes d'ami causées par les getters appelés de multiples fois. - Rechargement des deux composants à l'acceptation d'une demande d'ami afin de gérer le cas où deux personnes se sont mutuellement envoyées une requête.

--- assets/js/ajax/api/users/acceptFriendRequest.js
+++ assets/js/ajax/api/users/acceptFriendRequest.js
@@ -7,14 +7,18 @@ $(() => {
         const componentElement = document.getElementById('friend-requests-received-pagination');
         const component = await getComponent(componentElement);
 
+        const sentComponentElement = document.getElementById('friend-requests-sent-pagination');
+        const sentComponent = await getComponent(sentComponentElement);
+
         event.preventDefault();
         const id = $(this).attr('data-user-id');
 
         try {
             await api.put(`users/friend-request/${id}/accept`).json();
-            if (component) {
-                await component.render();
-            }
+            await Promise.all([
+                component?.render(),
+                sentComponent?.render()
+            ]);
         } catch (error) {
             new FlashMessage(error.message, FlashMessage.Types.ERROR);
         }
