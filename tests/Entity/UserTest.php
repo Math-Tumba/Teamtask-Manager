1b98b9fb60af3a61ee0dee165cbc09ee64eb04c8
Math Tumba <tumbarello.math@gmail.com>
2026-03-04 15:05:33 +0100

    Tests : Ajout de tests sur l'entité User.

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -4,27 +4,54 @@ namespace App\Tests\Entity;
 
 use App\Entity\User;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(User::class)]
 class UserTest extends TestCase
 {
-    public function testDefault()
+    public function testUserIsInstanciatedWithDefaultValues(): void
     {
         $user = new User();
 
-        $user
-            ->setUsername('tototata')
-            ->setEmail('tato@gmail.com')
-            ->setPassword('hashPassword')
-            ->setName('tata')
-            ->setSurname('toto')
-            ->setCountry('FR');
+        $this->assertContains('ROLE_USER', $user->getRoles());
+        $this->assertEquals(User::getDefaultProfilePicturePath(), $user->getProfilePicture());
+        $this->assertCount(0, $user->getFriendRequestsReceived());
+        $this->assertCount(0, $user->getFriendRequestsSent());
+        $this->assertCount(0, $user->getFriends());
 
-        $this->assertSame('tototata', $user->getUsername());
-        $this->assertSame('tato@gmail.com', $user->getEmail());
-        $this->assertSame('tata', $user->getName());
-        $this->assertSame('toto', $user->getSurname());
-        $this->assertSame('FR', $user->getCountry());
+        return;
+    }
+
+
+
+    public function testUsernameIsSameAsUserIdentifier(): void
+    {
+        $user = new User();
+        $user->setUsername('toto');
+
+        $this->assertEquals($user->getUserIdentifier(), $user->getUsername());
+
+        return;
+    }
+
+
+    #[DataProvider('rolesForUser')]
+    public function testUserHasAlwaysRoleUser(array $input, array $expected): void
+    {
+        $user = new User();
+        $user->setRoles($input);
+
+        $this->assertEqualsCanonicalizing($expected, $user->getRoles());
+
+        return;
+    }
+    public static function rolesForUser(): array
+    {
+        return [
+            'No role' => [[], ['ROLE_USER']],
+            'Give admin role' => [['ROLE_ADMIN'], ['ROLE_ADMIN', 'ROLE_USER']],
+            'Duplicated user role' => [['ROLE_USER'], ['ROLE_USER']],
+        ];
     }
 }
